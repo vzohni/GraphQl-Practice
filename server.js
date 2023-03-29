c96ff0a9f70bf5aeefc10057ac102c02752aeed8
Victor Zohni <vzohni@gmail.com>
2023-03-29 19:58:18 -0400

    update

--- server.js
+++ server.js
@@ -1,34 +1,85 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt, GraphQLNonNull } = require("graphql");
+const { resolve } = require("path");
 
 const app = express();
 
-
 //dummy data
 const authors = [
-	{ id: 1, name: 'J. K. Rowling' },
-	{ id: 2, name: 'J. R. R. Tolkien' },
-	{ id: 3, name: 'Brent Weeks' }
-]
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" },
+];
 const books = [
-	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
-	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
-	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
-	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
-	{ id: 5, name: 'The Two Towers', authorId: 2 },
-	{ id: 6, name: 'The Return of the King', authorId: 2 },
-	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
-]
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represents an Author of a book",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
+  }),
+});
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book, written by an author",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => {
+        return authors.find((author) => author.id === book.authorId);
+      },
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    book: {
+      type: BookType,
+      description: "a single book",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      description: "list of books",
+      resolve: () => books,
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "list of authors",
+      resolve: () => authors,
+    },
+  }),
+});
 
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "HelloWorld",
-    fields: () => ({
-      message: { type: GraphQLString, resolve: () => "Hello World" },
-    }),
-  }),
+  query: RootQueryType,
 });
 
 app.use(
